107953d928621c534ae8509f9e4f64602a0cbf73
edward-shandy <edward.shandy@ui.ac.id>
2024-11-26 20:44:36 +0700

    adding POM

--- cypress/e2e/orange-hrm/login.cy.js
+++ cypress/e2e/orange-hrm/login.cy.js
@@ -8,9 +8,9 @@ describe('login feature', () => {
 
     //TC-01
     it('Pengguna dapat membuka halaman login', () => {
-      cy.get('img[alt="company-branding"]').should('be.visible');
-      cy.get('div.orangehrm-login-logo img').should('exist');
-      cy.get('h5').should('contain', 'Login');
+      loginPage.companyBranding().should('be.visible');
+      loginPage.orangeLogo().should('exist');
+      loginPage.copytextLogin().should('contain', 'Login');
     })
 
     //TC-02
@@ -20,70 +20,70 @@ describe('login feature', () => {
 
     //TC-03
     it('Pengguna dapat menginput password', () => {
-      cy.get('input[name="password"]').should('exist').type('admin123');
+      loginPage.inputPassword().should('exist').type('admin123');
     })
 
     //TC-04
     it('Pengguna tidak dapat melakukan login tanpa input username', () => {
       loginPage.inputUsername().should('exist').clear().should('have.value', '');
-      cy.get('input[name="password"]').should('exist').type('admin123');
-      cy.get('button[type="submit"]').click();
-      cy.get('span.oxd-input-field-error-message').should('contain','Required');
+      loginPage.inputPassword().should('exist').type('admin123');
+      loginPage.submitButton().click();
+      loginPage.errorMessageRequired().should('contain','Required');
     })
 
     //TC-05
     it('Pengguna tidak dapat melakukan login tanpa input password', () => {
       loginPage.inputUsername().should('exist').type('Admin');
-      cy.get('input[name="password"]').should('exist').clear().should('have.value', '');
-      cy.get('button[type="submit"]').click();
-      cy.get('span.oxd-input-field-error-message').should('contain','Required');
+      loginPage.inputPassword().should('exist').clear().should('have.value', '');
+      loginPage.submitButton().click();
+      loginPage.errorMessageRequired().should('contain','Required');
     })
     
     //TC-06
     it('Pengguna tidak dapat melakukan login tanpa input username dan password', () => {
-      cy.get('button[type="submit"]').should('exist').click();
-      cy.get('span.oxd-input-field-error-message').should('contain','Required');
+      loginPage.submitButton().should('exist').click();
+      loginPage.errorMessageRequired().should('contain','Required');
     })
 
     //TC-07
     it('Pengguna tidak dapat melakukan login dengan username yang salah', () => {
       loginPage.inputUsername().should('exist').type('Admin-test');
-      cy.get('input[name="password"]').should('exist').type('admin123');
-      cy.get('button[type="submit"]').click();
-      cy.get('p.oxd-alert-content-text').should('contain','Invalid credentials');
+      loginPage.inputPassword().should('exist').type('admin123');
+      loginPage.submitButton().click();
+      loginPage.alertInvalidCredential().should('contain','Invalid credentials');
     })
 
     //TC-08
     it('Pengguna tidak dapat melakukan login dengan password yang salah', () => {
       loginPage.inputUsername().should('exist').type('Admin');
-      cy.get('input[name="password"]').should('exist').type('admin123-test');
-      cy.get('button[type="submit"]').click();
-      cy.get('p.oxd-alert-content-text').should('contain','Invalid credentials');
+      loginPage.inputPassword().should('exist').type('admin123-test');
+      loginPage.submitButton().click();
+      loginPage.alertInvalidCredential().should('contain','Invalid credentials');
     })
 
     //TC-09
     it('Pengguna tidak dapat melakukan login dengan username dan password yang mengandung spesial karakter', () => {
       loginPage.inputUsername().should('exist').type('@Admin');
-      cy.get('input[name="password"]').should('exist').type('@admin123-test');
-      cy.get('button[type="submit"]').click();
-      cy.get('p.oxd-alert-content-text').should('contain','Invalid credentials');
+      loginPage.inputPassword().should('exist').type('@admin123-test');
+      loginPage.submitButton().click();
+      loginPage.alertInvalidCredential().should('contain','Invalid credentials');
     })
 
     //TC-10
     it('Pengguna berhasil melakukan login', () => {
       loginPage.inputUsername().should('exist').type('Admin');
-      cy.get('input[name="password"]').should('exist').type('admin123');
+      loginPage.inputPassword().should('exist').type('admin123');
       cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/action-summary').as('actionSummary');
       cy.intercept('GET', '**/shortcuts').as('shortcuts');
       cy.intercept('GET', '**/subunit').as('subunit');
       cy.intercept('GET', '**/locations').as('locations');
       cy.intercept('GET', '**/time-at-work*').as('timeAtWork');
-      cy.get('button[type="submit"]').click();
+      loginPage.submitButton().click();
       cy.wait('@actionSummary');
       cy.wait('@shortcuts');
       cy.wait('@subunit');
       cy.wait('@locations');
       cy.wait('@timeAtWork');
-      cy.get('span.oxd-main-menu-item--name').should('contain','Dashboard');
+      loginPage.dashboardMenu().should('contain','Dashboard');
     })
 })
--- cypress/pom/login/login-pom.cy.js
+++ cypress/pom/login/login-pom.cy.js
@@ -1,5 +1,37 @@
 export default class loginPage {
+    static companyBranding(){
+        return cy.get('img[alt="company-branding"]');
+    }
+
+    static orangeLogo(){
+        return cy.get('div.orangehrm-login-logo img');
+    }
+
+    static copytextLogin(){
+        return cy.get('h5');
+    }
+
     static inputUsername(){
-        return cy.get('input[name="username"]')
+        return cy.get('input[name="username"]');
+    }
+
+    static inputPassword(){
+        return cy.get('input[name="password"]');
+    }
+
+    static submitButton(){
+        return cy.get('button[type="submit"]');
+    }
+
+    static errorMessageRequired(){
+        return cy.get('span.oxd-input-field-error-message');
+    }
+
+    static alertInvalidCredential(){
+        return cy.get('p.oxd-alert-content-text');
+    }
+
+    static dashboardMenu(){
+        return cy.get('span.oxd-main-menu-item--name');
     }
 }
